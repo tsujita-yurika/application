752e972510aa808972bb2d79ab5bf46e61d57cc0
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-09-05 09:05:16 +0000

    pause3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
   #ストロングパラメータ(セキュリティのためつける。privateから書く。最後のendの上に置く)
   def list_params #モデル名_paramsと名付ける
