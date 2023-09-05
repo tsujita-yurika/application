54119888aea513c82d2328ccbdadde60472a5013
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-09-05 14:07:08 +0000

    pause3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,14 +5,11 @@ class ListsController < ApplicationController
   end
 
   def create
-    #すること4つ↓
-    # 1&2. データを受け取り新規登録するためのインスタンス作成。
-    list = List.new(list_params)
-    # 3.データをデータベースに保存するsaveメソッド実行
-    list.save
-    #4. トップ画面へリダイレクト→詳細画面へリダイレクトに変更
-    # redirect_to '/top'
-    redirect_to list_path(list.id)
+      @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
   end
 
   def index
@@ -44,6 +41,7 @@ class ListsController < ApplicationController
   def list_params #モデル名_paramsと名付ける
     params.require(:list).permit(:title, :body)
   end
+  
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
